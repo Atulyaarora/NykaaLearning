7e060a973848810fdd58be6846317e49e0e36da4
Atulyaarora <atulya.03@gmail.com>
2019-09-19 12:27:03 +0530

    first

---
+++ Object Repository/Login_objects/Click_account.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Click_account</name>
+   <tag></tag>
+   <elementGuidId>e9337757-8207-425e-8ec6-9f3c70bfac25</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//span[@class='AccountText']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
